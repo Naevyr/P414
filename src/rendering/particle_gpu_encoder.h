1dbbe7a66e1488cfd5e0ca3ff3de55bc8bc3375f
Naevyr <volpenera12@gmail.com>
2023-07-06 17:38:43 +0200

    Add future support for shader octree

--- src/rendering/particle_gpu_encoder.h
+++ src/rendering/particle_gpu_encoder.h
@@ -2,7 +2,7 @@
 
 #include "core/particle.h"
 #include "core/particle_buffer.h"
-
+#include <godot_cpp/classes/texture3d.hpp>
 
 #include <godot_cpp/variant/packed_byte_array.hpp>
 #include <godot_cpp/classes/camera3d.hpp>
@@ -18,10 +18,15 @@ namespace PSS
         /// Ordered by distance to point
         /// </summary>
         public: 
+        static godot::PackedByteArray encode_octree_particle_buffer (godot::Ref<ParticleBuffer> buffer);
+        static godot::Texture3D encode_octree_octans(godot::Ref<ParticleBuffer> buffer);
+        static godot::PackedByteArray encode_particle_positions (godot::Ref<ParticleBuffer> buffer,godot::Vector3 cameraPosition);
+        static godot::PackedByteArray encode_particle_colors (godot::Ref<ParticleBuffer> buffer,godot::Vector3 cameraPosition);
+
+
+
         static godot::PackedByteArray encode_buffer_ordered (godot::Ref<ParticleBuffer> buffer,godot::Vector3 cameraPosition);
-
         static godot::PackedByteArray encode_particle(Particle particle,godot::Ref<ParticleBuffer> buffer);
-
         static godot::PackedByteArray encode_camera(godot::Camera3D * camera);
       
 
